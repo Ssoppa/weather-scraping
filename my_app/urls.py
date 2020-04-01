b0fbfe0815be66508f80e86973808c8dfc071802
Ssoppa <suppersoppa@gmail.com>
2020-04-01 12:07:34 -0300

    added pages links, base html frontend

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, contact, about, new_search
 
 urlpatterns = [
     path('', home),
+    path('contact', contact),
+    path('about', about),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -3,5 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'my_app/index.html')
 
+
+def contact(request):
+    return render(request, 'my_app/contact.html')
+
+
+def about(request):
+    return render(request, 'my_app/about.html')
+
+
+def new_search(request):
+    return render(request, 'my_app/about.html')
