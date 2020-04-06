50bc138c114be65a541e66106a298d8d5b27fa2c
Ssoppa <suppersoppa@gmail.com>
2020-04-06 11:51:50 -0300

    test3 heroku deploy

--- weather-scrape/urls.py
+++ weather-scrape/urls.py
@@ -1,4 +1,4 @@
-"""Weather URL Configuration
+"""weather-scrape URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
